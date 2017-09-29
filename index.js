bf56d26a6464d9b38b905182ad93d4ece6bae9a8
David Yim <yimd85@gmail.com>
2017-09-28 21:39:24 -0400

    first commit

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ app.set('view engine','pug');
 app.set('views','./my-views')
 
 
+
+
 app.get('/:id',function(request,response){
-  var userId=request.params.id;
   fs.readFile('./countries.json','utf-8',function(err,jsonFile){
     if(err){
       throw error;
     }
     var data = JSON.parse(jsonFile);
+    var userId=request.params.id;
     var dataWTF = data[userId];
-    response.render('list',dataWTF)
+    response.render('list',data[userId])
 
   })
 })
 
 app.get('*',function(request,response){
-  response.status(400).send('Type in adress "http://localhost:3000/"+3 digit country ISO code')
+  response.status(404).send('uh oh! page not found!')
 })
 
 app.listen(3000,function(){
