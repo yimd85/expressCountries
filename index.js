e59d57461672c77833a3b2cea61c87c6e5a5e7f7
David Yim <yimd85@gmail.com>
2017-09-30 20:07:12 -0400

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Please type in adress "http://localhost:3000/"+3 digit country ISO code
+// Please type in adress "http://localhost:3000/country"+3 digit country ISO code (needs to be in CAPS)
 
 var express = require('express')
 var fs= require('fs');
@@ -10,7 +10,7 @@ app.set('views','./my-views')
 
 
 
-app.get('/:id',function(request,response){
+app.get('/country/:id',function(request,response){
   fs.readFile('./countries.json','utf-8',function(err,jsonFile){
     if(err){
       throw error;
@@ -24,7 +24,7 @@ app.get('/:id',function(request,response){
 })
 
 app.get('*',function(request,response){
-  response.status(404).send('uh oh! page not found!')
+  response.status(404).send('Please type in adress "localhost:3000/country/"+3 digit country ISO code (needs to be in CAPS)')
 })
 
 app.listen(3000,function(){
